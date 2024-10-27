012d6dbc85521b469a7c03be78513bd0da50cde0
Mainul Hasan Khan <72866491+TechMainul-dev@users.noreply.github.com>
2024-10-27 21:25:17 +0600

    add directive

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -20,25 +20,26 @@ function formateName(value: string) {
 })
 export class UserProfileComponent {
   // @Input() name = '';
+
   @Input({ alias: 'userName', transform: formateName }) name = '';
-  @Input({ transform: booleanAttribute }) isHidden!: boolean;
+  @Input({ transform: booleanAttribute }) isHidden: boolean = false;
   @Input({ transform: numberAttribute }) pin!: number;
 
-  // name = `Sadia`;
-  status = `Married`;
-  salary = 10000;
-  isBtnDisabled = false;
-  inputVal = 'test';
+  // // name = `Sadia`;
+  // status = `Married`;
+  // salary = 10000;
+  // isBtnDisabled = false;
+  // inputVal = 'test';
 
-  users = [
-    { name: 'Mainul', isSingle: false, salary: 50000 },
-    { name: 'Hasan', isSingle: false, salary: 20000 },
-    { name: 'Khan', isSingle: false, salary: 30000 },
-  ];
+  // users = [
+  //   { name: 'Mainul', isSingle: false, salary: 50000 },
+  //   { name: 'Hasan', isSingle: false, salary: 20000 },
+  //   { name: 'Khan', isSingle: false, salary: 30000 },
+  // ];
 
-  onInput(e: Event) {
-    const value = (e.target as HTMLInputElement).value;
-    this.inputVal = value;
-    console.log(value);
-  }
+  // onInput(e: Event) {
+  //   const value = (e.target as HTMLInputElement).value;
+  //   this.inputVal = value;
+  //   console.log(value);
+  // }
 }
